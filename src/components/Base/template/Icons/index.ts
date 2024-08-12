47f8f6858a54035b02f8501365d649f19ac11481
HoangPham2510 <hoang.pham@tomosia.com>
2024-08-12 11:17:09 +0700

    feature/miap-cms-102 update ui checkout screen

--- src/components/Base/template/Icons/index.ts
+++ src/components/Base/template/Icons/index.ts
@@ -1,8 +1,31 @@
+import ArrowRightIcon from "./ArrowRightIcon.vue";
+import ArrowLeftIcon from "./ArrowLeftIcon.vue";
 import BagIcon from "./BagIcon.vue";
 import DropdownIcon from "./DropdownIcon.vue";
+import DiscountIcon from "./DiscountIcon.vue";
+import EditIcon from "./EditIcon.vue";
 import FilterIcon from "./FilterIcon.vue";
 import HomeIcon from "./HomeIcon.vue";
 import HeartIcon from "./HeartIcon.vue";
+import NoteIcon from "./NoteIcon.vue";
+import MinusIcon from "./MinusIcon.vue";
 import NotifyIcon from "./NotifyIcon.vue";
+import PlusIcon from "./PlusIcon.vue";
+import WalletIcon from "./WalletIcon.vue";
 
-export { BagIcon, DropdownIcon, FilterIcon, HomeIcon, HeartIcon, NotifyIcon };
+export {
+  ArrowRightIcon,
+  ArrowLeftIcon,
+  BagIcon,
+  DropdownIcon,
+  DiscountIcon,
+  EditIcon,
+  FilterIcon,
+  HomeIcon,
+  HeartIcon,
+  MinusIcon,
+  NotifyIcon,
+  PlusIcon,
+  NoteIcon,
+  WalletIcon,
+};
